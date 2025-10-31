29f3882dd0a45f07e38b800ad89ba17b1cdfea8b
nandofree2 <nandofree2@gmail.com>
2025-10-31 23:29:43 +0700

    add help user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def admin?
+    current_user&.role&.name == 'admin'
+  end
+
+  def staff?
+    current_user&.role&.name == 'staff'
+  end
+
+  def customer?
+    current_user&.role&.name == 'customer'
+  end
+
+  def user_name
+    current_user&.name
+  end
+
+  def owner_of?(comment)
+    current_user && current_user.id == comment.user_id
+  end
+
 end
